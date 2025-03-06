c45223d49901fa8796051e0071a255d3e764d870
ehonis <ethanhonisb@gmail.com>
2025-03-06 12:36:19 -0500

    twoSumJS Solution

--- twoSum.js
+++ twoSum.js
@@ -3,14 +3,36 @@
  * @param {number} target
  * @return {number[]}
  */
+
+//O(n^2)
 var twoSum = function (nums, target) {
   for (const [index, num] of nums.entries()) {
     if (nums.includes(target - num)) {
       const foundIndex = nums.indexOf(target - num);
 
+      if (foundIndex === index) {
+        continue;
+      }
       return [index, foundIndex];
     }
   }
 };
 
-console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([3, 3], 6));
+
+//O(n)
+var nTwoSum = function (nums, target) {
+  const map = new Map();
+
+  for (const [index, num] of nums.entries()) {
+    const compliment = target - num;
+
+    if (map.has(compliment)) {
+      return [map.get(compliment), index];
+    }
+
+    map.set(num, index);
+  }
+};
+
+console.log(nTwoSum([3, 3], 6));
